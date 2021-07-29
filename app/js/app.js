c7e5d1bb316991492923a13b2988e9f44fbb03bf
doniyor11 <udoniyorbek99@mail.ru>
2021-07-29 18:13:39 +0500

    pages

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,7 @@ import 'swiper/swiper-bundle.min'
 import 'particles.js/particles'
 // import 'particles.js/demo/js/lib/stats'
 import $ from 'jquery'
+
 window.jQuery = $
 window.$ = $
 
@@ -14,12 +15,12 @@ window.$ = $
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+    // Custom JS
     $('a[href*="#"]')
         // Remove links that don't actually link to anything
         .not('[href="#"]')
         .not('[href="#0"]')
-        .click(function(event) {
+        .click(function (event) {
             // On-page links
             if (
                 location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
@@ -27,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 location.hostname == this.hostname
             ) {
                 // Figure out element to scroll to
-                var target = $(this.hash);
+                let target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 // Does a scroll target exist?
                 if (target.length) {
@@ -35,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     event.preventDefault();
                     $('html, body').animate({
                         scrollTop: target.offset().top
-                    }, 1000, function() {
+                    }, 1000, function () {
                         // Callback after animation
                         // Must change focus!
                         var $target = $(target);
@@ -43,17 +44,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         if ($target.is(":focus")) { // Checking if the target was focused
                             return false;
                         } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
                             $target.focus(); // Set focus again
-                        };
+                        }
+                        ;
                     });
                 }
             }
         });
 
 
-
-    const swiper = new Swiper('.swiper-container', {
+    const swiper = new Swiper('.services-swiper', {
         direction: 'horizontal',
         slidesPerView: 3,
         spaceBetween: 100,
@@ -69,6 +70,10 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesPerView: 2,
         spaceBetween: 40,
         loop: true,
+        simulateTouch: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -83,6 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesPerView: 4,
         spaceBetween: 70,
         loop: true,
+        simulateTouch: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
         autoplay: {
             delay: 5000,
         },
@@ -92,124 +101,159 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
 
-
-
-    /* ---- particles.js config ---- */
-
-    particlesJS("particles-js", {
-        "particles": {
-            "number": {
-                "value": 380,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#567dd9"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 5
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 3,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 150,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 6,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 800,
-                    "rotateY": 1200
-                }
-            }
+    const swiper4 = new Swiper('.p-swiper ', {
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        simulateTouch: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
+        autoplay: {
+            delay: 5000,
         },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": "grab"
-                },
-                "onclick": {
-                    "enable": true,
-                    "mode": "push"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 140,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
-        "retina_detect": true
     });
+
+
+    const swiper5 = new Swiper('.s-swiper ', {
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        simulateTouch: true,
+        touchRatio: 1,
+        touchAngle: 45,
+        grabCursor: true,
+        autoplay: {
+            delay: 5000,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
 
 })
 
 
+/* ---- particles.js config ---- */
+
+// particlesJS("particles-js", {
+//     "particles": {
+//         "number": {
+//             "value": 380,
+//             "density": {
+//                 "enable": true,
+//                 "value_area": 800
+//             }
+//         },
+//         "color": {
+//             "value": "#567dd9"
+//         },
+//         "shape": {
+//             "type": "circle",
+//             "stroke": {
+//                 "width": 0,
+//                 "color": "#000000"
+//             },
+//             "polygon": {
+//                 "nb_sides": 5
+//             },
+//             "image": {
+//                 "src": "img/github.svg",
+//                 "width": 100,
+//                 "height": 100
+//             }
+//         },
+//         "opacity": {
+//             "value": 0.5,
+//             "random": false,
+//             "anim": {
+//                 "enable": false,
+//                 "speed": 1,
+//                 "opacity_min": 0.1,
+//                 "sync": false
+//             }
+//         },
+//         "size": {
+//             "value": 3,
+//             "random": true,
+//             "anim": {
+//                 "enable": false,
+//                 "speed": 40,
+//                 "size_min": 0.1,
+//                 "sync": false
+//             }
+//         },
+//         "line_linked": {
+//             "enable": true,
+//             "distance": 150,
+//             "color": "#ffffff",
+//             "opacity": 0.4,
+//             "width": 1
+//         },
+//         "move": {
+//             "enable": true,
+//             "speed": 6,
+//             "direction": "none",
+//             "random": false,
+//             "straight": false,
+//             "out_mode": "out",
+//             "bounce": false,
+//             "attract": {
+//                 "enable": false,
+//                 "rotateX": 800,
+//                 "rotateY": 1200
+//             }
+//         }
+//     },
+//     "interactivity": {
+//         "detect_on": "canvas",
+//         "events": {
+//             "onhover": {
+//                 "enable": true,
+//                 "mode": "grab"
+//             },
+//             "onclick": {
+//                 "enable": true,
+//                 "mode": "push"
+//             },
+//             "resize": true
+//         },
+//         "modes": {
+//             "grab": {
+//                 "distance": 140,
+//                 "line_linked": {
+//                     "opacity": 1
+//                 }
+//             },
+//             "bubble": {
+//                 "distance": 400,
+//                 "size": 40,
+//                 "duration": 2,
+//                 "opacity": 8,
+//                 "speed": 3
+//             },
+//             "repulse": {
+//                 "distance": 200,
+//                 "duration": 0.4
+//             },
+//             "push": {
+//                 "particles_nb": 4
+//             },
+//             "remove": {
+//                 "particles_nb": 2
+//             }
+//         }
+//     },
+//     "retina_detect": true
+// });
 
 
 //
